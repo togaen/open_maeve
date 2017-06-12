0276030e40bc185229ceb54e9e29378a6e8e25eb
Jeffrey Kane Johnson <contact@maeveautomation.com>
2017-06-12 11:30:48 -0700

    CD-10 convert python script comments to doxygen style

--- build_utility_scripts/scripts/merge_compile_commands.py
+++ build_utility_scripts/scripts/merge_compile_commands.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
-# When using catkin tools with ROS, the compile_commands.json files are
-# generated per package. This script collects them all and dumps them into a
-# single file at the root of the workspace. This is needed for the
-# YouCompleteMe vim plugin.
 # Copyright (C) 2017 Sebastian Klose - All Rights Reserved
 # Permission to copy and modify is granted under the GPLv3 license
 
 import os
 import json
 
-workspace_path = os.path.expanduser('~') + '/catkin_ws'
+## @package merge_compile_commands
+# Helper script for YouCompleteMe vim plugin.
+#
+# When using catkin tools with ROS, the compile_commands.json files are
+# generated per package. This script collects them all and dumps them into a
+# single file at the root of the workspace. This is needed for the
+# YouCompleteMe vim plugin.
 
+workspace_path = os.path.expanduser('~') + '/catkin_ws'
 compile_command_filename = 'compile_commands.json'
 root = workspace_path
 output_file = root + '/' + compile_command_filename
