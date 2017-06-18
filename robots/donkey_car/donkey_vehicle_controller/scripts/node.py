51b53c3db32bcde3c212431af4058f480f4a8583
Jeffrey Kane Johnson <contact@maeveautomation.com>
2017-06-17 23:16:34 -0700

    [donkey_vehicle_controller] pep8 format

--- robots/donkey_car/donkey_vehicle_controller/scripts/node.py
+++ robots/donkey_car/donkey_vehicle_controller/scripts/node.py
@@ -10,10 +10,12 @@ from controller_interface_msgs.msg import LatLonCommand
 ## @package donkey_vehicle_controller
 # Vehicle controller for the donkey car.
 
+
 def map_index(idx, r1_min, r1_max, r2_min, r2_max):
     alpha = (idx - r1_min) / (r1_max - r1_min)
     r2_offset = alpha * (r2_max - r2_min)
     return int(r2_min + r2_offset)
+
 
 class Controller:
     RANGE_MIN = -1
@@ -25,15 +27,26 @@ class Controller:
 
     def update(self, cmd):
         if cmd < 0:
-            pulse = map_index(cmd, self.RANGE_MIN, 0, self.params['min_pulse'], self.params['zero_pulse'])
+            pulse = map_index(
+                cmd,
+                self.RANGE_MIN,
+                0,
+                self.params['min_pulse'],
+                self.params['zero_pulse'])
         else:
-            pulse = map_index(cmd, 0, self.RANGE_MAX, self.params['zero_pulse'], self.params['max_pulse'])
+            pulse = map_index(
+                cmd,
+                0,
+                self.RANGE_MAX,
+                self.params['zero_pulse'],
+                self.params['max_pulse'])
 
         #self.controller.set_pulse(pulse)
         print 'set ' + str(pulse) + ' to channel ' + str(self.params['channel'])
 
 
 class Handler:
+
     def __init__(self, p):
         self.throttle_controller = Controller(p.throttle_actuator)
         self.steering_controller = Controller(p.steering_actuator)
@@ -47,5 +60,8 @@ if __name__ == '__main__':
     node_params = ros_parameter_loading.NodeParams()
 
     handler = Handler(node_params)
-    rospy.Subscriber(node_params.lat_lon_command_topic, LatLonCommand, handler.callback)
+    rospy.Subscriber(
+        node_params.lat_lon_command_topic,
+        LatLonCommand,
+     handler.callback)
     rospy.spin()
