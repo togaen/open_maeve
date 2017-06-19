1fdeb879a5457f5b6e46bccccc77cecb117bc558
Jeffrey Kane Johnson <contact@maeveautomation.com>
2017-06-18 20:41:40 -0700

    [DT-1] add comments to donkey vehicle controller node

--- robots/donkey_car/donkey_vehicle_controller/scripts/node.py
+++ robots/donkey_car/donkey_vehicle_controller/scripts/node.py
@@ -11,20 +11,43 @@ from controller_interface_msgs.msg import Command2D
 # Vehicle controller for the donkey car.
 
 
-def map_index(idx, r1_min, r1_max, r2_min, r2_max):
-    alpha = (idx - r1_min) / (r1_max - r1_min)
-    r2_offset = alpha * (r2_max - r2_min)
-    return int(r2_min + r2_offset)
+##
+# @brief Map an index `idx` of range 1 onto range 2.
+#
+# @note Behavior undefined if idx \notin [range1_min, range1_max] or if either
+# range is invalid.
+#
+# @param idx An index into range 1.
+# @param range1_min Minimum of range 1.
+# @param range1_max Maximum of range 1.
+# @param range2_min Minimum of range 2.
+# @param range2_max Maximum of range 2.
+#
+# @return The index into range 2 whose offset is proportional to that of idx into range 1.
+def map_index(idx, range1_min, range1_max, range2_min, range2_max):
+    alpha = (idx - range1_min) / (range1_max - range1_min)
+    range2_offset = alpha * (range2_max - range2_min)
+    return int(range2_min + range2_offset)
 
 
+##
+# @brief Convenience wrapper for loading a servo controller and performing command scaling.
 class Controller:
     RANGE_MIN = -1
     RANGE_MAX = 1
 
+    ##
+    # @brief Instantiate a servo controller and keep a local copy of the params.
+    #
+    # @param p The param set for the servo controller.
     def __init__(self, p):
         self.controller = adafruit_pca9685.PCA9685_Controller(p['channel'])
         self.params = p
 
+    ##
+    # @brief Map a command `cmd` \in [-1,1] onto [min_pulse,max_pulse] centered on `zero_pulse`.
+    #
+    # @param cmd The input control command.
     def update(self, cmd):
         if cmd < 0:
             pulse = map_index(
@@ -47,12 +70,23 @@ class Controller:
             self.controller.set_pulse(pulse)
 
 
+##
+# @brief Handler class for control command callback.
 class Handler:
+
+    ##
+    # @brief Instantiate controllers for throttle and steering.
+    #
+    # @param p Servo params.
 
     def __init__(self, p):
         self.throttle_controller = Controller(p.throttle_actuator)
         self.steering_controller = Controller(p.steering_actuator)
 
+    ##
+    # @brief Read control command from message, send to controller.
+    #
+    # @param msg The control command message.
     def callback(self, msg):
         self.throttle_controller.update(msg.x)
         self.steering_controller.update(msg.y)
