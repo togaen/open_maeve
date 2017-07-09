d4d7709a33d9bec058d93edea643611d8b9778ed
Maeve-Pi <maeve-pi@maeveautomation.com>
2017-07-09 09:17:27 -0700

    [CD-19] donkey honk now listens to topic to activate
    --HG--
    branch : CD-19-donkey-honk

--- robots/donkey_car/donkey_honk/scripts/donkey_honk_node.py
+++ robots/donkey_car/donkey_honk/scripts/donkey_honk_node.py
@@ -3,6 +3,7 @@
 # Permission to copy and modify is granted under the MIT license
 
 import rospy
+from std_msgs.msg import Bool
 
 import ros_parameter_loading
 import donkey_honk
@@ -11,13 +12,38 @@ import donkey_honk
 # Vehicle controller for the donkey car.
 
 
+##
+# @brief Handler class for donkey honk callback.
+class Handler:
+
+    ##
+    # @brief Instantiate the piezo buzzer control.
+    #
+    # @param p Buzzer params.
+    def __init__(self, p):
+        self.buzzer = donkey_honk.Buzzer(
+            p.buzzer_gpio_pin,
+            p.buzzer_duration,
+            p.buzzer_on_duration,
+            p.buzzer_off_duration)
+
+    ##
+    # @brief Activate the buzzer for each 'True' signal on the topic.
+    #
+    # @param msg The activation message.
+    def callback(self, msg):
+        if not msg.data:
+            return
+
+        self.buzzer.buzz()
+
 if __name__ == '__main__':
     rospy.init_node('donkey_honk')
     node_params = ros_parameter_loading.NodeParams()
 
-    buzzer = donkey_honk.Buzzer(
-        node_params.buzzer_gpio_pin,
-        node_params.buzzer_duration,
-     node_params.buzzer_on_duration,
-     node_params.buzzer_off_duration)
-    buzzer.buzz()
+    handler = Handler(node_params)
+    rospy.Subscriber(
+        node_params.buzzer_topic,
+        Bool,
+     handler.callback)
+    rospy.spin()
