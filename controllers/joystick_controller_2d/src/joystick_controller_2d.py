deaa5e84b0c9abd8d2fb6e62796aabac068beca8
Jeffrey Kane Johnson <contact@maeveautomation.com>
2017-10-30 10:14:28 -0700

    [joystick_controller_2d] move parameters up; add optional gains

--- controllers/joystick_controller_2d/src/joystick_controller_2d.py
+++ controllers/joystick_controller_2d/src/joystick_controller_2d.py
@@ -7,17 +7,22 @@ from controller_interface_msgs.msg import Command2D
 ## @package joystick_controller_2d
 # Mapping functionality from joystick message to Command2D message
 
+
+##
+# @brief Parameters used by the script to transform from joystick to Command2D.
+JOYSTICK_X_AXIS = 1
+JOYSTICK_Y_AXIS = 3
+JOYSTICK_X_GAIN = 1.0
+JOYSTICK_Y_GAIN = 1.0
+
 ##
 # @brief Map a joystick message to a Command2D message.
 #
 # The function map up/down motions of the left joystick hat to x axis
 # commands, and the left/right motions of the right joystick hat to y axis
-# commands. The default axis mapping is tested on one controller, adjust as
-# needed.
+# commands.
 #
 # @param m The input joystick message.
-# @param x_axis The raw joystick axis index for x axis control commands.
-# @param y_axis The raw joystick axis index for y axis control commands.
 #
 # @return The equivalent Command2D message.
 
@@ -26,6 +31,6 @@ def JoyMsgToCommand2DMsg(m, x_axis=3, y_axis=1):
     cmd_msg = Command2D()
     cmd_msg.header = m.header
     cmd_msg.lazy_publishing = True
-    cmd_msg.x = m.axes[y_axis]
-    cmd_msg.y = m.axes[x_axis]
+    cmd_msg.x = JOYSTICK_X_GAIN * m.axes[JOYSTICK_X_AXIS]
+    cmd_msg.y = JOYSTICK_Y_GAIN * m.axes[JOYSTICK_Y_AXIS]
     return cmd_msg
