38bab57e585d439b91fc5e4398ff24e61cb91d97
Jeffrey Kane Johnson <contact@maeveautomation.com>
2017-11-01 16:01:28 -0700

    [isp_controller_2d] add horizon visualizer object
    --HG--
    branch : CD-48-horizon-viz

--- controllers/isp_controller_2d/include/maeve_automation_core/isp_controller_2d/ros_interface.h
+++ controllers/isp_controller_2d/include/maeve_automation_core/isp_controller_2d/ros_interface.h
@@ -21,12 +21,17 @@
  */
 #pragma once
 
-#include "controller_interface_msgs/Command2D.h"
-#include "ros/ros.h"
-#include "std_msgs/Header.h"
+#include <image_transport/image_transport.h>
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
 
+#include <string>
+#include <unordered_map>
+
+#include "controller_interface_msgs/Command2D.h"
 #include "maeve_automation_core/isp_controller_2d/control_command.h"
 #include "maeve_automation_core/isp_controller_2d/isp_controller_2d.h"
+#include "maeve_automation_core/ros_parameter_loading/params_base.h"
 
 namespace maeve_automation_core {
 /**
@@ -64,4 +69,82 @@ ControlCommand command2D_Msg2ControlCommand(
 bool loadISP_ControllerROS_Params(const ros::NodeHandle& nh,
                                   const std::string& ns,
                                   ISP_Controller2D::Params& params);
+
+/**
+ * @brief Helper class for visualizing control horizons.
+ */
+class HorizonVisualizer {
+ public:
+  /**
+   * @brief Parameter container for horizon visualizer.
+   */
+  struct Params {
+    /** @brief Pixel height of the horizon visualization. */
+    int horizon_viz_height;
+    /** @brief Range min for soft constraint values. */
+    double constraint_range_min;
+    /** @brief Range max for soft constraint values. */
+    double constraint_range_max;
+
+    /**
+     * @brief Whether the parameter object has reasonable values.
+     *
+     * @return True if the values are reasonable; otherwise false.
+     */
+    bool valid() const;
+
+    /**
+     * @brief Constructor: initialize to invalid values.
+     */
+    Params();
+
+    /**
+     * @brief Explicit constructor.
+     *
+     * @param viz_height Height of visualization to generate.
+     * @param r_min Soft constraint range min.
+     * @param r_max Soft constraint range max.
+     */
+    Params(const int viz_height, const double r_min, const double r_max);
+  };  // struct Params
+
+  /**
+   * @brief Initialize the visualization object.
+   *
+   * @param params Parameter set for the visualizer.
+   * @param horizons The set of horizons to generalize visualizations for.
+   * @param it The parent node's image transport object.
+   */
+  void initialize(const Params& params,
+                  const std::vector<std::string>& horizons,
+                  image_transport::ImageTransport& it);
+
+  /**
+   * @brief Generate and publish visualizations for the horizons.
+   *
+   * @param header The message header to use.
+   * @param controller The ISP controller that holds the horizons.
+   */
+  void visualize(const std_msgs::Header& header,
+                 const ISP_Controller2D& controller) const;
+
+ private:
+  /** @brief Parameter object for visualizer. */
+  Params params_;
+
+  /**
+   * @brief Helper function for visualizing horizon structures.
+   *
+   * @param header Message header for published horizon visualization.
+   * @param horizon The horizon to visualize.
+   * @param ht The horizon type enum.
+   * @param publisher The publisher to use to publish the visualization.
+   */
+  void visualizeHorizon(const std_msgs::Header& header, const cv::Mat& horizon,
+                        const ISP_Controller2D::HorizonType ht,
+                        const image_transport::Publisher& publisher) const;
+
+  /** @brief Horizon visualization publishers. */
+  std::unordered_map<std::string, image_transport::Publisher> viz_horizon_pubs_;
+};  // class HorizonVisualizer
 }  // namespace maeve_automation_core
