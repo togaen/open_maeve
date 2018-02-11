45e41074bca9e5a130567fa603c102743ad22a78
Jeffrey Kane Johnson <contact@maeveautomation.org>
2018-02-10 19:20:41 -0500

    rough unit tests for reachability
    --HG--
    branch : CD-59-maeve-dynamics

--- maeve_dynamics/test/test_pst_reachability.cpp
+++ maeve_dynamics/test/test_pst_reachability.cpp
@@ -24,7 +24,71 @@
 #include "maeve_automation_core/maeve_dynamics/pst_reachability.h"
 
 namespace maeve_automation_core {
-TEST(Maeve_Dynamics_PST_Reachability, tesTargetTerminalSpeed) {
-  EXPECT_TRUE(false);
+TEST(Maeve_Dynamics_PST_Reachability, testTargetTerminalSpeed) {
+  {
+    const Eigen::Vector2d p1(3.0, 2.0);
+    const Eigen::Vector2d p2(7.0, 7.5);
+    auto t_bounds = Interval(0.0, 10.0);
+    auto s_bounds = Interval(0.0, 10.0);
+    auto s_dot_bounds = Interval(0.0, 5.0);
+    auto s_ddot_bounds = Interval(-1.0, 2.0);
+    const auto constraints = IntervalConstraints<2>(
+        std::move(t_bounds), {s_bounds, s_dot_bounds, s_ddot_bounds});
+
+    const auto speed = Interval::min(s_dot_bounds);
+    ASSERT_NO_THROW({
+      const auto reachability = PST_Reachability::compute(
+          Interval(speed, speed), p1, p2, constraints);
+      if (reachability) {
+        std::cout << PST_Reachability::reachableInterval(*reachability)
+                  << std::endl;
+        const auto& min_connector =
+            PST_Reachability::minConnector(*reachability);
+        const auto& max_connector =
+            PST_Reachability::maxConnector(*reachability);
+        std::cout << "min: " << min_connector << std::endl;
+        std::cout << "max: " << max_connector << std::endl;
+      } else {
+        std::cout << "Not reachable for initial speed: " << speed << std::endl;
+      }
+    });
+  }
+
+#if 0
+compute(
+      const Interval& I_i, const Eigen::Vector2d& p1, const Eigen::Vector2d& p2,
+      const IntervalConstraints<2>& constraints)
+
+  IntervalConstraints(Interval&& t_bounds,
+                      std::array<Interval, Order + 1>&& s_bounds);
+
+#endif
+  const Eigen::Vector2d p1(3.0, 2.0);
+  const Eigen::Vector2d p2(7.0, 7.5);
+  auto t_bounds = Interval(0.0, 10.0);
+  auto s_bounds = Interval(0.0, 10.0);
+  auto s_dot_bounds = Interval(0.0, 5.0);
+  auto s_ddot_bounds = Interval(-1.0, 2.0);
+  const auto constraints = IntervalConstraints<2>(
+      std::move(t_bounds), {s_bounds, s_dot_bounds, s_ddot_bounds});
+
+  const auto start_speed = Interval::min(s_dot_bounds);
+  const auto speed_inc = 0.01;
+  auto speed = start_speed;
+  const auto end_speed = Interval::max(s_dot_bounds);
+  while (speed <= end_speed) {
+    ASSERT_NO_THROW({
+      const auto reachability = PST_Reachability::compute(
+          Interval(speed, speed), p1, p2, constraints);
+      if (reachability) {
+        std::cout << PST_Reachability::reachableInterval(*reachability)
+                  << std::endl;
+      } else {
+        std::cout << "Not reachable for initial speed: " << speed << std::endl;
+      }
+    });
+
+    speed += speed_inc;
+  }
 }
 }  // namespace maeve_automation_core
