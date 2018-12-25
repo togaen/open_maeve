29b894e65168d4416023d89407320e1155d3a1d8
Jeffrey Kane Johnson <jeffrey.kane.johnson@pm.me>
2018-12-25 11:56:12 -0500

    flesh out package framework
    --HG--
    branch : kitti-stereo-publisher

---
+++ data_processing/karlsruhe_dataset_publisher/src/karlsruhe_dataset_publisher/karlsruhe_dataset_publisher_node_handler.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2018 Maeve Automation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+#pragma once
+
+#include <nav_msgs/Odometry.h>
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+
+namespace maeve_automation_core {
+class KarlsruheDatasetPublisherNodeHandler {
+ public:
+  /**
+   * @brief Construct the ROS interface.
+   *
+   * @param params The ROS node params.
+   * @param nh The ROS node handle.
+   */
+  KarlsruheDatasetPublisherNodeHandler(const ros::NodeHandle& nh);
+
+  /**
+   * @brief Publish the odometry and left/right image messages.
+   */
+  void publish();
+
+ private:
+};  // class KarlsruheDatasetPublisherNodeHandler
+}  // namespace maeve_automation_core
