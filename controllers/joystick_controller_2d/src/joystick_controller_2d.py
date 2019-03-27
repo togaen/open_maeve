4fa6ab3756987b1ff372bbdbbee420a4f7f9731d
Jeffrey Kane Johnson <jeffrey.kane.johnson@pm.me>
2019-03-27 13:52:25 -0400

    [joystick_controller_2d] allow joystick params to be set at run time

--- controllers/joystick_controller_2d/src/joystick_controller_2d.py
+++ controllers/joystick_controller_2d/src/joystick_controller_2d.py
@@ -9,30 +9,21 @@ from controller_interface_msgs.msg import Command2D
 
 
 ##
-# @brief Parameters used by the script to transform from joystick to Command2D.
-JOYSTICK_X_AXIS = 1
-JOYSTICK_Y_AXIS = 0
-JOYSTICK_X_GAIN = 1.0
-JOYSTICK_Y_GAIN = 1.0
-
-##
 # @brief Map a joystick message to a Command2D message.
 #
 # The function map up/down motions of the left joystick hat to x axis
 # commands, and the left/right motions of the right joystick hat to y axis
 # commands.
 #
-# @param m The input joystick message.
-#
 # @return The equivalent Command2D message.
 
 
 def JoyMsgToCommand2DMsg(
         m,
-        x_axis=JOYSTICK_X_AXIS,
-        x_gain=JOYSTICK_Y_GAIN,
-        y_axis=JOYSTICK_Y_AXIS,
-        y_gain=JOYSTICK_Y_GAIN):
+        x_axis,
+        x_gain,
+        y_axis,
+        y_gain):
     cmd_msg = Command2D()
     cmd_msg.header = m.header
     cmd_msg.sticky_control = True
