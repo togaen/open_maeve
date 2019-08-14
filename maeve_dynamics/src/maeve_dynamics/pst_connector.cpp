32838af0b911032a9e4730e0c0c1f19db86281db
Jeffrey Kane Johnson <jeffrey.kane.johnson@pm.me>
2019-08-14 15:27:55 -0400

    [maeve_dynamics] adjust EPS, add todo

--- maeve_dynamics/src/maeve_dynamics/pst_connector.cpp
+++ maeve_dynamics/src/maeve_dynamics/pst_connector.cpp
@@ -445,7 +445,8 @@ bool PST_Connector::segmentsConnected(const PST_Connector& connector) {
 
   // Absolute error for doing approximate floating point comparisons.
   // TODO(me): This should be a parameter.
-  constexpr auto EPS = 5e-4;
+  // TODO(me): desparately need to be computing some kind of relative error
+  constexpr auto EPS = 1e-2;
 
   // The path values at t1 and at t2 should be equal.
   const auto connected = (approxEq(s01, s11, EPS) && approxEq(s12, s22, EPS));
