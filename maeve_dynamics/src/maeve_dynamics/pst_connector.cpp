004bbe53b7d67090fb8535fa745056020430365f
Jeffrey Kane Johnson <jeffrey.kane.johnson@pm.me>
2019-09-02 13:49:33 -0400

    [maeve_dynamics] epsilon tuning

--- maeve_dynamics/src/maeve_dynamics/pst_connector.cpp
+++ maeve_dynamics/src/maeve_dynamics/pst_connector.cpp
@@ -446,7 +446,7 @@ bool PST_Connector::segmentsConnected(const PST_Connector& connector) {
 
   // Absolute error for doing approximate floating point comparisons.
   // TODO(me): This should be a parameter.
-  constexpr auto EPS = 1e-5;
+  constexpr auto EPS = 1e-4;
 
   // The path values at t1 and at t2 should be equal.
   const auto connected =
